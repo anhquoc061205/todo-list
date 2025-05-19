bdb8789c8b8ba4df636e32d69195ff91d3ed8397
anhquoc061205 <kanekyken918@gmail.com>
2025-05-19 21:01:05 +0700

    add list, confirm list

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-console.log("Hello");
+const todoInput = document.getElementById("todo-input");
+const addButton = document.getElementById("add-button");
+
 document.addEventListener("DOMContentLoaded", getTodos);
+addButton.addEventListener("click", addTodo);
+
 // function getTodos(){
 //    fetch("https://6627a8e6b625bf088c0930ad.mockapi.io/tasks")
 // //    chuyển về json
@@ -18,11 +22,14 @@ document.addEventListener("DOMContentLoaded", getTodos);
 // console.log(response);
 // }
 
-
+// Get function
 async function getTodos(){
  try{
     const response = await axios.get("https://6627a8e6b625bf088c0930ad.mockapi.io/tasks");
-console.log(response.data);
+
+
+const ul = document.querySelector(".todo-list");
+ul.innerHTML = "";
 response.data.forEach((item) => {
     console.log(item);
     // console.log(item.content);
@@ -45,11 +52,11 @@ li.innerHTML = `
                         </div>
                     </div>
                     <div class="todo-actions">
-                        <button><i class="fa-solid fa-pen-to-square"></i></button>
+                        <button ><i class="fa-solid fa-pen-to-square"></i></button>
                         <button><i class="fa-solid fa-trash"></i></button>
                     </div>
                 </li>`;
-                   const ul = document.querySelector(".todo-list");
+                   
 //    gắn thẻ li vào ul
    ul.appendChild(li);
     
@@ -61,4 +68,65 @@ li.innerHTML = `
  }catch(err){
       console.log(err);
  }
+}
+
+
+// Post function
+async function addTodo(){
+    let inputData = todoInput.value.trim();
+    if (!inputData) return;
+    const newTodo = {
+    createdAt: new Date().toISOString(),
+    content: inputData,
+    isCompleted: false,
+    
+    };
+
+   try{
+   
+    await axios.post("https://6627a8e6b625bf088c0930ad.mockapi.io/tasks", newTodo);
+
+   todoInput.value = "";
+//    fetch Data
+    getTodos();
+    // Alert notification
+    Swal.fire({
+      title: "có cái lol",
+      width: 600,
+      padding: "3em",
+      color: "#716add",
+      background: "#fff url(https://sweetalert2.github.io/images/trees.png)",
+      backdrop: `
+    rgba(0,0,123,0.4)
+    url("https://sweetalert2.github.io/images/nyan-cat.gif")
+    left top
+    no-repeat
+  `,
+    });
+   }catch (error) {
+    console.log(error);
+   }
+
+
+}
+
+const handelUpdate(id, content){
+    Swal.fire({
+  title: "Submit your Github username",
+  input: "text",
+  inputAttributes: {
+    autocapitalize: "off"
+  },
+  inputValue: content,
+  showCancelButton: true,
+  confirmButtonText: "Look up",
+  showLoaderOnConfirm: true,
+  preConfirm: async (dataInput) => {
+  axios.put(`https://6627a8e6b625bf088c0930ad.mockapi.io/tasks/${id}`,
+    {
+        content: dataInput,
+    } 
+    );
+  },
+});
 }
